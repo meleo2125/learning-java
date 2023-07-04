3253070171c00856c50194e2214a7431047130b7
meleo <mukeshprajapat3093@gmail.com>
2023-07-04 15:58:45 +0530

    files added

--- exercise/ExForLoop9.java
+++ exercise/ExForLoop9.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class ExForLoop9 {
     public static void main(String[] args) {
+        //palindrome
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter a string : ");
