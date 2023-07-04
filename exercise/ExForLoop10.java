69e9bc32d38627fe54057e18d7b6145537029b34
meleo <mukeshprajapat3093@gmail.com>
2023-07-04 15:58:55 +0530

    files added

---
+++ exercise/ExForLoop10.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ExForLoop10 {
+    public static void main(String[] args) {
+        //number pattern
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a integer : ");
+        int n = input.nextInt();
+
+        for (int i = 1;i <= n;i++){
+            for (int j = 1;j <= i;j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ exercise/ExForLoop11.java
@@ -0,0 +1,5 @@
+public class ExForLoop11 {
+    public static void main(String[] args) {
+
+    }
+}
